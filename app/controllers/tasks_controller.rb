70f9e1347b382c3fdcb4712ab7a38aefe6d986d4
sukekou <sukekou1031@gmail.com>
2018-08-20 15:20:25 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
+  before_action :require_user_logged_in
   
   def index
     @tasks = Task.all
@@ -9,15 +11,15 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスク が正常に登録されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'タスク が登録できませんでした'
       render :new
@@ -42,7 +44,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'タスク は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to  root_url
   end
   
   private
@@ -54,6 +56,13 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'toppages/index'
+
+  root to: 'toppages#index'
 
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 end
